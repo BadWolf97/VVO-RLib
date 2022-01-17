2f3b2ac288028d010452d624f2025b6abdf332f1
Nicolas Reuter <nicolas@fphst.de>
2022-01-17 12:50:43 +0100

    added Imports to DESCRIPTION, added Roxygen-Comments

--- R/VVOLib.R
+++ R/VVOLib.R
@@ -1,20 +1,12 @@
-# Needed Packages
-vvo_require <- function() {
-  install.packages(c("jsonlite", "httr", "tidyverse", "leaflet", "shiny", "shinydashboard", "anytime"))
-  library(jsonlite)  #Handling of JSON-Strings
-  library(httr)  #Handling of HTTP-Requests
-  library(tidyverse)  #Tidyverse eben
-  library(leaflet)  #To present a good Map
-  library(shiny)  #Dashboard
-  library(shinydashboard)  #Dashboard
-  library(anytime)  #Handling of Date/Time-Components
-}
-
 #Helper-Method for getting results via the WebAPI
+#' @importFrom httr POST add_headers content
+#' @importFrom jsonlite toJSON fromJSON
 vvo_get <- function(endpoint, body) {
+  #Format Request to a JSON
   request_body_json <- toJSON(body, auto_unbox = TRUE)
   print(paste("RequestBody:", request_body_json))
   print(paste("Using Endpoint", paste("https://webapi.vvo-online.de", endpoint, sep = "/"), "..."))
+  # Do the Request
   post_result <- POST(paste("https://webapi.vvo-online.de", endpoint, sep = "/"),
                       body = request_body_json,
                       add_headers(
@@ -23,10 +15,14 @@ vvo_get <- function(endpoint, body) {
                           "charset"="utf-8")
                       )
   )
+  # Return the Request
   return (fromJSON(content(post_result, as = "text")))
 }
 
 #Helper-Method for getting a Date from the stuff the API sends
+#' @importFrom anytime anytime
+#' @importFrom GGally %>%
+#' @importFrom stringr str_replace fixed
 vvo_handleDate <- function(date) {
   return (anytime(as.numeric(str_replace(date, fixed("/Date("), "") %>% str_replace(fixed("-0000)/"), "")) / 1000))
 }
@@ -36,17 +32,28 @@ vvo_toDate <- function(date) {
   return (paste0("/Date(", round(as.numeric(date) * 1000) , "-0000)/"))
 }
 
-#Helper-Method to beautify Platform-Description
+#' @title Helper-Method to beautify Platform-Description
+#' @param Platform Platformobject from other Functions
+#' @return String with Steig or Gleis depending on Platformtype
+#' @export
+#' @importFrom GGally %>%
+#' @importFrom stringr str_replace fixed
 vvo_transformPlatformType <- function(Platform) {
-  return (paste(Platform$Type, Platform$Name) %>% 
+  return (paste(Platform$Type, Platform$Name) %>%
             str_replace(fixed("Platform"), "Steig") %>%
             str_replace(fixed("Railtrack"), "Gleis")
   )
 }
 
-#Get Stops by searchquery
+#' @title Get Stops in VVO based on a query
+#' @param query Query to search with. Supports Abbrevations by DVB
+#' @return Tibble with Informations about the Stop
+#' @export
+#' @importFrom GGally %>%
+#' @importFrom dplyr as_tibble
+#' @importFrom purrr set_names
 vvo_getStops <- function(query) {
-  result <- vvo_get("tr/pointfinder", 
+  result <- vvo_get("tr/pointfinder",
                     list(query = query, stopsOnly = TRUE))
   df_result <- read.table(text = unlist(result$Points), sep = "|", header = FALSE) %>%
     as_tibble() %>%
@@ -54,11 +61,17 @@ vvo_getStops <- function(query) {
   # NA in City means it is in Dresden
   df_result$City[is.na(df_result$City)] = "Dresden"
   # Drop NA or otherwise unknown result
-  df_result = subset(df_result, select = -c(Type, Distance, Unknown, ShortCode))
+  df_result = subset(df_result, select = -c("Type", "Distance", "Unknown", "ShortCode"))
   return(df_result)
 }
 
-#Get Departures by StopID
+#' @title Get Departures by StopID
+#' @param stopid StopID (can be obtained by vvo_getStops)
+#' @param limit Resultcount. Default 10, Maximum (due to API-Restrictions) 50
+#' @return Tibble with Informations about the Departures
+#' @export
+#' @importFrom GGally %>%
+#' @importFrom dplyr as_tibble mutate
 vvo_getDeps <- function(stopid, limit = 10){
   result <- vvo_get("dm", list(stopid = stopid, limit = limit, stopsOnly = TRUE))
   df_result = as_tibble(result$Departures) %>%
@@ -70,7 +83,14 @@ vvo_getDeps <- function(stopid, limit = 10){
   return(df_result)
 }
 
-#Get a Trip by StopID, TripID and Time
+#' @title Get a Trip by StopID, TripID and Time
+#' @param stopid StopID (can be obtained by vvo_getStops)
+#' @param tripid TripID (can be obtained by vvo_getDeps). Note that its not an ID for a specific trip, but for a route (e.g. all Trips on line 1 from Prohlis to Leutewitz have the same ID)
+#' @param time Departuretime of the specific trip on the specified station. Default is current time.
+#' @return Tibble with Informations about all Stations in the Trip
+#' @export
+#' @importFrom GGally %>%
+#' @importFrom dplyr as_tibble mutate
 vvo_getTrip <- function(stopid, tripid, time = Sys.time()) {
   result <- vvo_get("dm/trip", list(tripid = tripid, stopid = stopid, time = vvo_toDate(time)))
   df_result = as_tibble(result$Stops) %>%
@@ -82,12 +102,20 @@ vvo_getTrip <- function(stopid, tripid, time = Sys.time()) {
   return(df_result)
 }
 
-#Get a Trip by a Departure from vvo_getDeps
+#' @rdname vvo_getTrip
+#' @title ... or by StopID and Departure
+#' @param stopid StopID (can be obtained by vvo_getStops)
+#' @param departure One observation from vvo_getDeps
+#' @export
 vvo_getTripByDep <- function(stopid, departure) {
   return(vvo_getTrip(stopid, departure$Id, departure$ScheduledTime))
 }
 
-#Get all RouteChanges in VVO
+#' @title Get all RouteChanges in VVO
+#' @return Tibble with all Changes in the VVO.
+#' @export
+#' @importFrom GGally %>%
+#' @importFrom dplyr as_tibble mutate
 vvo_getRouteChanges <- function() {
   result <- vvo_get("rc", list(shortterm = TRUE))
   df_result = as_tibble(result$Changes) %>%
@@ -98,21 +126,31 @@ vvo_getRouteChanges <- function() {
   return(df_result)
 }
 
-#Get Lines at Stop by StopID
+#' @title Get Lines at Stop by StopID
+#' @param stopid StopID (can be obtained by vvo_getStops)
+#' @return Tibble with all Lines at this stop.
+#' @export
+#' @importFrom dplyr as_tibble
 vvo_getLinesAtStop <- function(stopid) {
   result <- vvo_get("stt/lines", list(stopid = stopid))
   df_result = as_tibble(result$Lines)
   return(df_result)
 }
 
-#Get all Stops (daily updated)
+#' @title Get all Stops (daily updated)
+#' @return Tibble with all Stops in VVO, fetched from an open_data-JSON. There might be duplicates.
+#' @source https://www.vvo-online.de/open_data/VVO_STOPS.JSON
+#' @export
+#' @importFrom GGally %>%
+#' @importFrom dplyr as_tibble mutate
+#' @importFrom jsonlite fromJSON
 vvo_getAllStops <- function() {
   df_result = fromJSON("https://www.vvo-online.de/open_data/VVO_STOPS.JSON") %>%
     as_tibble() %>%
     mutate(
       x = as.numeric(x),
       y = as.numeric(y),
-      
+
       #MOTs = unique(Lines$Vehicle),
     )
   return(df_result)
